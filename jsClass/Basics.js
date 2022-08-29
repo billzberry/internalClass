dd560343883c38f96ed756e02dc24be70675578b
oseiGyapong <oseigyapong@logig.tech>
2022-08-29 14:50:55 +0000

    Update

---
+++ jsClass/Basics.js
@@ -0,0 +1,44 @@
+var myclass = "Logig Training One";
+let myNewName = 'Osei Gyapong';
+const myWorkPlace = "LoGig";
+
+let name = 'Bernice'
+let age = 23
+let school = 'TTU'
+
+let details = 'My name is ' + name + ', I am ' + age + ' years old and I attend ' + school
+let newDetails = `My name is ${name} I am ${age} years old and I attend ${school}`
+
+let addTwoValues = 5 % 3
+
+let yearOfBirth = 2000
+let year = new Date().getUTCFullYear()
+let myAge = year - yearOfBirth
+
+let myBoolean = false
+
+let myArray = ['1st item', '2nd items', 20, '4th item']
+let list = [
+    'dhjfjfjgjgjgj fhfhfh fbfhf',
+    'dshgdfghfd fhfghfg ufgjgf fg',
+    'ghghg fjgjgj gjgjg'
+]
+
+// String
+// Array
+// Object
+// Number
+// Float
+// Double
+// Boolean
+
+// console.log(myclass)
+// console.log(myNewName)
+// console.log(myWorkPlace)
+// console.log(details)
+// console.log(newDetails)
+// console.log(addTwoValues)
+// console.log(myAge)
+console.log(myArray.length)
+console.log(myArray[3])
+console.log(myArray)
